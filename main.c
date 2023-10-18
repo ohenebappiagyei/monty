9a50fef9ceb42c1a31757349ee928806bf41e077
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-10-18 09:31:44 +0000

    Implemented pop

--- main.c
+++ main.c
@@ -55,6 +55,10 @@ int main(int argc, char *argv[])
 	{
 		pint(&stack, line_number);
 	}
+	else if (strcmp(opcode, "pop") == 0)
+        {
+            pop(&stack, line_number);
+        }
         else
         {
             fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
